9e8567062143c40b9883691c15ada5ccb40fbd02
JuJoGil <91592897+JuJoGil@users.noreply.github.com>
2023-05-17 12:39:47 +0200

    THE END

--- Assets/Scripts/LVL 4/RockDamage.cs
+++ Assets/Scripts/LVL 4/RockDamage.cs
@@ -11,7 +11,7 @@ public class RockDamage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Controller.Singleton.Life = 3;
     }
 
     // Update is called once per frame
